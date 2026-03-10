24aa8e12079c501da9a4edd5caf857fe2902cfff
w83220719 <w83220719@outlook.com>
2026-03-10 22:55:07 +0800

    style: 统一代码格式并添加.clang-format配置文件
    添加.clang-format配置文件统一代码风格，调整函数声明和定义格式
    重构代码使其符合新的代码风格规范，包括：
    - 统一指针和引用符号位置
    - 简化函数声明和定义格式
    - 移除多余的空行和括号

--- csrc/include/utils.h
+++ csrc/include/utils.h
@@ -3,21 +3,15 @@
 
 #include <torch/extension.h>
 
-template <typename T>
-inline void CheckCuda(T &x)
-{
+template <typename T> inline void CheckCuda(T& x) {
     TORCH_CHECK(x.device().is_cuda(), "Must be on CUDA device");
 }
 
-template <typename T>
-inline void CheckContiguous(T &x)
-{
+template <typename T> inline void CheckContiguous(T& x) {
     TORCH_CHECK(x.is_contiguous(), "Input tensor must be contiguous.");
 }
 
-template <typename T>
-inline void CheckInput(T &x)
-{
+template <typename T> inline void CheckInput(T& x) {
     CheckCuda(x);
     CheckContiguous(x);
 }
