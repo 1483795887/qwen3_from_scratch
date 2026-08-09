b06f18abc3c25e73e984212d8a2cf3b97615795a
w83220719 <1483795887@qq.com>
2026-08-09 09:34:10 +0800

    feat: 实现调度器的预填充部分及其测试

--- src/qwen3_from_scratch/inference/scheduler.py
+++ src/qwen3_from_scratch/inference/scheduler.py
@@ -22,9 +22,44 @@ class Scheduler:
         self.block_manager = BlockManager(num_blocks=config.max_blocks, block_size=config.block_size)
 
     def add_request(self, req: Sequence):
+        if len(req.prompts) > self.max_num_tokens:
+            return False
         self.waiting.append(req)
+        return True
+
+    def schedule_prefill(self) -> list[Sequence]:
+        scheduled_reqs = []
+        batched_tokens = 0
+
+        waiting_nums = len(self.waiting)
+        # 记录队列中的长度，因为后续会修改队列
+        for i in range(waiting_nums):
+            seq = self.waiting[0]
+            self.waiting.popleft()
+            remaining_tokens = self.max_num_tokens - batched_tokens
+            if len(seq.prompts) > remaining_tokens:
+                # 不满足的放到队尾，不会再次被遍历到
+                self.waiting.append(seq)
+                continue
+            if not seq.block_tables:
+                if not self.block_manager.can_allocate(seq):
+                    self.waiting.append(seq)
+                    continue
+                self.block_manager.allocate(seq)
+            
+            seq.status = SequenceStatus.RUNNING
+            self.active.append(seq)
+            scheduled_reqs.append(seq)
+            batched_tokens += len(seq)
+            if (len(scheduled_reqs) >= self.max_num_seqs) or len(self.waiting) == 0:
+                break
+        return scheduled_reqs
 
     def schedule(self) -> list[Sequence]:
+        scheduled_reqs = self.schedule_prefill()
+
+        if scheduled_reqs:
+            return scheduled_reqs
         return []
 
     def post_process(self, seqs: list[Sequence], token_ids: list[int], is_prefill: bool):
--- test/inference/test_scheduler.py
+++ test/inference/test_scheduler.py
@@ -1,4 +1,5 @@
 from qwen3_from_scratch.inference.scheduler import SchedulerConfig, Scheduler
+from qwen3_from_scratch.inference.sequence import Sequence, SequenceStatus
 
 
 def make_scheduler(num_pages: int = 100, block_size: int = 16, max_num_seqs: int = 8, max_num_tokens=100) -> Scheduler:
@@ -12,14 +13,98 @@ def make_scheduler(num_pages: int = 100, block_size: int = 16, max_num_seqs: int
 
 
 class TestAddRequest:
-    pass
+    def test_empty_queue_when_init(self):
+        scheduler = make_scheduler()
+        assert len(scheduler.active) == 0
+        assert len(scheduler.waiting) == 0
 
+    def test_return_one_seq_when_add(self):
+        scheduler = make_scheduler()
+        seq = Sequence([0] * 32)
+        scheduler.add_request(seq)
+        assert len(scheduler.active) == 0
+        assert len(scheduler.waiting) == 1
+
+    def test_return_false_when_prompts_more_than_max_num_tokens(self):
+        scheduler = make_scheduler(max_num_tokens=100)
+        seq = Sequence([0]* 101)
+        assert not scheduler.add_request(seq)
 
 class TestSchedule:
     def test_return_no_seq_when_init(self):
         scheduler = make_scheduler()
         seqs = scheduler.schedule()
         assert len(seqs) == 0
+
+    def test_return_added_seq(self):
+        scheduler = make_scheduler()
+        seq = Sequence([0]* 32)
+        scheduler.add_request(seq)
+        scheduled_seqs = scheduler.schedule()
+        assert len(scheduled_seqs) == 1
+        scheduled_seq = scheduled_seqs[0]
+        assert scheduled_seq.req_id == seq.req_id
+
+    def test_fill_seq_when_schedule_prefill(self):
+        scheduler = make_scheduler()
+        seq = Sequence([0] * 32)
+        assert seq.status == SequenceStatus.WAITING
+        scheduler.add_request(seq)
+        scheduled_seqs = scheduler.schedule()
+        assert len(scheduled_seqs) == 1
+        scheduled_seq = scheduled_seqs[0]
+        assert scheduled_seq.req_id == seq.req_id
+        assert scheduled_seq.block_tables is not None
+        assert scheduled_seq.is_prefill
+        assert scheduled_seq.status == SequenceStatus.RUNNING
+
+    def test_restricted_by_max_seq_num(self):
+        scheduler = make_scheduler(max_num_tokens=1000, max_num_seqs=2)
+        for _ in range(100):
+            scheduler.add_request(Sequence([0]*2))
+        reqs = scheduler.schedule()
+        assert len(reqs) == 2
+        
+    def test_restricted_by_max_seq_num_twice(self):
+        scheduler = make_scheduler(max_num_tokens=1000, max_num_seqs=2)
+        for _ in range(100):
+            scheduler.add_request(Sequence([0]*2))
+        reqs = scheduler.schedule()
+        assert len(reqs) == 2
+        reqs = scheduler.schedule()
+        assert len(reqs) == 2
+
+    def test_restricted_by_max_token_num(self):
+        scheduler = make_scheduler(max_num_tokens=5, max_num_seqs=1000)
+        for _ in range(100):
+            scheduler.add_request(Sequence([0] * 2))
+        reqs = scheduler.schedule()
+        assert len(reqs) == 2
+
+    def test_restricted_by_max_token_num_multiple_step(self):
+        scheduler = make_scheduler(max_num_tokens=5, max_num_seqs=1000)
+        for _ in range(3):
+            scheduler.add_request(Sequence([0] * 2))
+        reqs = scheduler.schedule()
+        assert len(reqs) == 2
+        reqs = scheduler.schedule()
+        assert len(reqs) == 1
+
+    def test_restricted_by_block_num(self):
+        scheduler = make_scheduler(num_pages=2)
+        scheduler.add_request(Sequence([0]* 32))
+        scheduler.add_request(Sequence([0]* 32))
+        reqs = scheduler.schedule()
+        assert len(reqs) == 1
+
+    def test_restricted_by_block_num_multiple_step(self):
+        scheduler = make_scheduler(num_pages=2)
+        scheduler.add_request(Sequence([0]* 32))
+        scheduler.add_request(Sequence([0]* 32))
+        reqs = scheduler.schedule()
+        assert len(reqs) == 1
+        reqs = scheduler.schedule()
+        assert len(reqs) == 0
 
 class TestPostProcess:
     pass
